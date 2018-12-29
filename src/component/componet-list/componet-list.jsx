9edfedde34567e4b09a11a03b0b870fab812793f
liuyi <runant@qq.com>
2018-12-29 16:04:16 +0800

    001

--- src/component/componet-list/componet-list.jsx
+++ src/component/componet-list/componet-list.jsx
@@ -14,7 +14,7 @@ class ComponentList extends Component {
     render() {
         return (
             <div className="col-md-8">
-                <h3 className="reply">评论回复：</h3>
+                <h3 className="reply">评论回复1：</h3>
                 <h2 style={{display: 'none'}}>暂无评论，点击左侧添加评论！！！</h2>
                 <ul className="list-group">
                     <li className="list-group-item">
