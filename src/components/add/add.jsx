5781c115d4e5759b76390091a8f2dc3cff376f7b
liuyi <runant@aliyun.com>
2019-01-02 17:00:40 +0800

    002

--- src/components/add/add.jsx
+++ src/components/add/add.jsx
@@ -10,41 +10,57 @@ export default class Add extends Component {
     addComment: PropTypes.func.isRequired
   }
 
-  submit = () => {
-    // 取出输入数据
-    const username = this.refs.username.value.trim()
-    const content = this.refs.content.value.trim()
-    // 合法性检查
-    if(!username || !content) {
-      return
-    }
-    // 封装成comment对象
-    const comment = {
-      username,
-      content
-    }
-    // 调用addComment添加comment
-    this.props.addComment(comment)
-    // 清除输入
-    this.refs.username.value = ''
-    this.refs.content.value = ''
+  state = {
+    username:'',
+      content:''
   }
 
+    handleSubmit =  () =>{
+        // 根据输入的数据创建评论对象
+        const comment =this.state
+        // 添加到comments中, 更新state
+        this.props.addComment(comment)
+        // 清除输入的数据
+        this.setState({
+            username: '',
+            content: ''
+        })
+    }
+
+
+    changeUsername =(event)=> {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    changeContent= (event)=> {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
   render () {
+      let {username,content} = this.state
     return (
+
       <div className="col-md-4">
         <form className="form-horizontal">
           <div className="form-group">
             <label>用户名</label>
-            <input type="text" className="form-control" placeholder="用户名" ref='username'/>
+            {/*<input type="text" className="form-control" placeholder="用户名" ref='username'/>*/}
+            <input type="text" className="form-control" placeholder="用户名"  value={username}
+                   onChange={this.changeUsername}/>
+
           </div>
           <div className="form-group">
             <label>评论内容</label>
-            <textarea className="form-control" rows="6" placeholder="评论内容" ref="content"></textarea>
+            {/*<textarea className="form-control" rows="6" placeholder="评论内容" ref="content"></textarea>*/}
+            <textarea className="form-control" rows="6" placeholder="评论内容" value={content} onChange={this.changeContent}></textarea>
           </div>
           <div className="form-group">
             <div className="col-sm-offset-2 col-sm-10">
-              <button type="button" className="btn btn-default pull-right" onClick={this.submit}>提交</button>
+              <button type="button" className="btn btn-default pull-right" onClick={this.handleSubmit}>提交</button>
             </div>
           </div>
         </form>
